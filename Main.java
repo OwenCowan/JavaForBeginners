a38d0a6ea7c2d9514911b099cb42f2954b95ed07
OwenCowan <78167052+OwenCowan@users.noreply.github.com>
2022-02-25 08:11:19 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.OWEN;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double dist;
+    dist = 7.2 * 1100;
+        System.out.println("The lightining is " + dist + " feet away");
+    }
+}
